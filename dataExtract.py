5c3276bd25511dfdf10a920049cff439de1f67e6
shighton <dime.bastian@gmail.com>
2023-05-09 16:26:23 -0400

    Pulling labels works

--- dataExtract.py
+++ dataExtract.py
@@ -5,6 +5,19 @@ import re
 with open('/Users/shighton/PycharmProjects/rnaML3/src/MHL_hsa_sep29_trnSet_miRNAgrpby_20_80.arff', 'r') as file:
     data = file.read()
 
-misses = re.search('0\\s\\%\\s\\w{22}\\&\\w{25}', data)
+labeled_misses = re.findall('0\\s%\\s\\w{22}&\\w{25}', data)
 
-misses.group()
+labeled_hits = re.findall('1\\s%\\s\\w{22}&\\w{25}', data)
+
+misses = ''
+hits = ''
+
+for miss in labeled_misses:
+    temp = re.search('\\w{22}&\\w{25}', miss)
+    misses += temp.group() + "\n"
+
+for hit in labeled_hits:
+    temp = re.search('\\w{22}&\\w{25}', hit)
+    hits += temp.group() + "\n"
+
+print(hits)
